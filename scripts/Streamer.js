8c5b0514a5b4cb0dd1c5b619a1b7e7e0396c9ca4
Tommy Sharkey <tommy.l.sharkey@gmail.com>
2020-11-05 13:47:20 -0800

    Adding VR support

--- scripts/Streamer.js
+++ scripts/Streamer.js
@@ -3,87 +3,89 @@ class Streamer {
     texture;
     uri;
 
-    // constructor(uri, scene) {
-    //     // Create a material from the video
-    //     let mat = new BABYLON.StandardMaterial("mat", scene);
-    //     this.texture = new BABYLON.VideoTexture("video", [uri], scene, true, false);
-    //     mat.diffuseTexture = this.texture;
-
-    //     this.mesh = BABYLON.Mesh.CreatePlane("videoPlane", 8, scene);
-    //     this.mesh.scaling.x = 1920 / 1080; // set aspect ratio
-    //     this.mesh.material = mat;
-    // }
-
     constructor(uri, scene) {
-        BABYLON.Effect.ShadersStore["customVertexShader"] = "\r\n" +
-            "precision highp float;\r\n" +
+        if (uri) {
+            // Create a material from the video
+            let mat = new BABYLON.StandardMaterial("mat", scene);
+            this.texture = new BABYLON.VideoTexture("video", [uri], scene, true, false);
+            mat.diffuseTexture = this.texture;
 
-            "// Attributes\r\n" +
-            "attribute vec3 position;\r\n" +
-            "attribute vec2 uv;\r\n" +
+            this.mesh = BABYLON.Mesh.CreatePlane("videoPlane", 8, scene);
+            this.mesh.scaling.x = 1920 / 1080; // set aspect ratio
+            this.mesh.material = mat;
+        }
 
-            "// Uniforms\r\n" +
-            "uniform mat4 worldViewProjection;\r\n" +
+        else {
+            BABYLON.Effect.ShadersStore["customVertexShader"] = "\r\n" +
+                "precision highp float;\r\n" +
 
-            "// Varying\r\n" +
-            "varying vec2 vUV;\r\n" +
+                "// Attributes\r\n" +
+                "attribute vec3 position;\r\n" +
+                "attribute vec2 uv;\r\n" +
 
-            "void main(void) {\r\n" +
-            "    gl_Position = worldViewProjection * vec4(position, 1.0);\r\n" +
+                "// Uniforms\r\n" +
+                "uniform mat4 worldViewProjection;\r\n" +
 
-            "    vUV = uv;\r\n" +
-            "}\r\n";
+                "// Varying\r\n" +
+                "varying vec2 vUV;\r\n" +
 
-        BABYLON.Effect.ShadersStore["customFragmentShader"] = "\r\n" +
-            "precision highp float;\r\n" +
+                "void main(void) {\r\n" +
+                "    gl_Position = worldViewProjection * vec4(position, 1.0);\r\n" +
 
-            "varying vec2 vUV;\r\n" +
+                "    vUV = uv;\r\n" +
+                "}\r\n";
 
-            "uniform sampler2D textureSampler;\r\n" +
+            BABYLON.Effect.ShadersStore["customFragmentShader"] = "\r\n" +
+                "precision highp float;\r\n" +
 
-            "void main(void) {\r\n" +
-            "    gl_FragColor = texture2D(textureSampler, vUV);\r\n" +
-            "}\r\n";
+                "varying vec2 vUV;\r\n" +
 
-        var myVideo; // Our Webcam stream (a DOM <video>)
-        var isAssigned = false; // Is the Webcam stream assigned to material?
+                "uniform sampler2D textureSampler;\r\n" +
 
-        var plane1 = BABYLON.Mesh.CreatePlane("streamer1", 7, scene);
-        plane1.rotation.z = Math.PI;
-        plane1.position.y = 1;
-        plane1.scaling.x = 1280 / 720; // set aspect ratio
-        this.mesh = plane1;
+                "void main(void) {\r\n" +
+                "    gl_FragColor = texture2D(textureSampler, vUV);\r\n" +
+                "}\r\n";
 
-        var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
-            vertex: "custom",
-            fragment: "custom",
-        }, {
-            attributes: ["position", "normal", "uv"],
-            uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
-        });
-        shaderMaterial.backFaceCulling = false;
+            var myVideo; // Our Webcam stream (a DOM <video>)
+            var isAssigned = false; // Is the Webcam stream assigned to material?
 
-        // Create our video texture
-        BABYLON.VideoTexture.CreateFromWebCam(scene, function (videoTexture) {
-            myVideo = videoTexture;
-            shaderMaterial.setTexture("textureSampler", myVideo);
-        }, {
-            maxWidth: 1280,
-            maxHeight: 720
-        });
+            var plane1 = BABYLON.Mesh.CreatePlane("streamer1", 7, scene);
+            plane1.rotation.z = Math.PI;
+            plane1.position.y = 1;
+            plane1.scaling.x = 1280 / 720; // set aspect ratio
+            this.mesh = plane1;
 
-        // When there is a video stream (!=undefined),
-        // check if it's ready          (readyState == 4),
-        // before applying shaderMaterial to avoid the Chrome console warning.
-        // [.Offscreen-For-WebGL-0xa957edd000]RENDER WARNING: there is no texture bound to the unit 0
-        scene.onBeforeRenderObservable.add(function () {
-            if (myVideo !== undefined && isAssigned == false) {
-                if (myVideo.video.readyState == 4) {
-                    plane1.material = shaderMaterial;
-                    isAssigned = true;
+            var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
+                vertex: "custom",
+                fragment: "custom",
+            }, {
+                attributes: ["position", "normal", "uv"],
+                uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
+            });
+            shaderMaterial.backFaceCulling = false;
+
+            // Create our video texture
+            BABYLON.VideoTexture.CreateFromWebCam(scene, function (videoTexture) {
+                myVideo = videoTexture;
+                shaderMaterial.setTexture("textureSampler", myVideo);
+            }, {
+                maxWidth: 1280,
+                maxHeight: 720
+            });
+
+            // When there is a video stream (!=undefined),
+            // check if it's ready          (readyState == 4),
+            // before applying shaderMaterial to avoid the Chrome console warning.
+            // [.Offscreen-For-WebGL-0xa957edd000]RENDER WARNING: there is no texture bound to the unit 0
+            scene.onBeforeRenderObservable.add(function () {
+                if (myVideo !== undefined && isAssigned == false) {
+                    if (myVideo.video.readyState == 4) {
+                        plane1.material = shaderMaterial;
+                        isAssigned = true;
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     play() {
--- scripts/main.js
+++ scripts/main.js
@@ -15,13 +15,31 @@ var createDefaultEngine = function () {
 var createScene = function () {
 	// Setup scene
 	var scene = new BABYLON.Scene(engine);
-	var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+	
+	var camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 4, 3, new BABYLON.Vector3(0, 3, 0), scene);
+	// var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 	camera.setTarget(BABYLON.Vector3.Zero());
 	camera.attachControl(canvas, true);
 	var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 	light.intensity = 0.7;
 
 	streamer = new Streamer("assets/samplevid.mp4", scene);
+
+	// enable VR
+	var vrHelper = scene.createDefaultVRExperience();
+	vrHelper.onAfterEnteringVRObservable.add(() => {
+		if (scene.activeCamera === vrHelper.vrDeviceOrientationCamera) {
+		  BABYLON.FreeCameraDeviceOrientationInput.WaitForOrientationChangeAsync(1000)
+			.then(() => {
+			  // Successfully received sensor input
+			})
+			.catch(() => {
+			  alert(
+				"Device orientation camera is being used but no sensor is found, prompt user to enable in safari settings"
+			  );
+			});
+		}
+	  });
 
 	return scene;
 };
